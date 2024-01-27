95f337490dc7df1fe796212ab708bd93c3ffeb53
Helio Vallim <h.vallim@gmail.com>
2024-01-26 22:17:40 -0300

    fix: ajuste na função playNextSong

--- script.js
+++ script.js
@@ -108,13 +108,15 @@ const pauseSong = () => {
 };
 
 const playNextSong = () => {
+
   if (userData?.currentSong === null) {
     playSong(userData?.songs[0].id);
   } else {
     const currentSongIndex = getCurrentSongIndex();
+    const nextSong = userData?.songs[currentSongIndex + 1];
+
+    playSong(nextSong.id);
   }
-  const nextSong = userData?.songs[currentSongIndex + 1];
-  playSong(nextSong.id);
 };
 
 const renderSongs = (array) => {
